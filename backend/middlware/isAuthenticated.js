2bd47412f211ecaae7dbe1db982716323c9ed24d
Deepak-singh123 <namanyadav334@gmail.com>
2025-02-18 15:00:34 +0530

    dependecy update backend

--- backend/middlware/isAuthenticated.js
+++ backend/middlware/isAuthenticated.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://bondifyy.netlify.app",
     credentials: true,
 }));
 
--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ const httpServer = createServer(app);
 // Initialize Socket.IO
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173", // Allow requests from your frontend
+        origin: "https://bondifyy.netlify.app", // Allow requests from your frontend
         credentials: true,
     },
 });
