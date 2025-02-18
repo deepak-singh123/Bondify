708c0e71824dcf83c1fada699ba9710a679db9a3
Deepak-singh123 <namanyadav334@gmail.com>
2025-02-18 14:55:28 +0530

    dependecy update backend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://bondifyy.netlify.app", // Frontend URL
     credentials: true,
 }));
 app.use(express.json());
