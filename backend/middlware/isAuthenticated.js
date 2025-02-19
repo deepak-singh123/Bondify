8dc78b518e5996fe8a4c91b953093751de7a778c
Deepak-singh123 <namanyadav334@gmail.com>
2025-02-19 10:09:53 +0530

    adding vercel

--- backend/middlware/isAuthenticated.js
+++ backend/middlware/isAuthenticated.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://bondify-ten.vercel.app",
+    origin: ["http://localhost:5173", "https://bondifyy.netlify.app"],
     credentials: true,
 }));
 
